34586e4e9c52c347908da4e6dac5d43864418367
ruchikapatro24 <ruchikapatro2004@gmail.com>
2025-05-30 17:03:43 +0530

    Resolve merge conflicts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="footer">
             <p>Developed with React & caffeine by <a href="https://www.linkedin.com/in/ruchika-patro-757b63258" target="_blank" rel="noopener noreferrer">Ruchika Patro</a></p>
-            <a href="" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+            <a href="https://github.com/ruchikapatro24/memeify" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
             <p>&copy; 2025 Memeify. All rights reserved.</p>
         </footer>
     );
